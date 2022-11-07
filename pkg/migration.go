298937d600d33c7df821721b2edc8f0e299e898b
otogushakov <otogushakov@ptsecurity.com>
2022-11-07 10:24:29 +0700

    Add migrations registry

---
+++ pkg/migration.go
@@ -0,0 +1,42 @@
+package dbshaker
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+
+	"github.com/ToggyO/dbshaker/internal"
+)
+
+func AddMigration(up internal.MigrationFunc, down internal.MigrationFunc) {
+	_, filename, _, _ := runtime.Caller(1)
+
+	version, err := internal.IsValidFileName(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	key := filepath.Dir(filename)
+	if err != nil {
+		panic(err)
+	}
+
+	folderRegistry, ok := registry[key]
+	if !ok {
+		folderRegistry = make(folderGoMigrationRegistry)
+	}
+
+	migration := &internal.Migration{
+		Name:    filename,
+		Version: version,
+		UpFn:    up,
+		DownFn:  down,
+	}
+
+	if exists, ok := folderRegistry[version]; ok {
+		logger.Fatal(fmt.Sprintf("failed to add migration %q: conflicts with exitsting %q", filename, exists.Name))
+	}
+
+	folderRegistry[version] = migration
+	registry[key] = folderRegistry
+}
